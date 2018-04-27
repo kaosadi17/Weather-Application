9419b89e52ab36f5d80ca0203308a004340f4a45
amalio <kaosadi17@gmail.com>
2018-04-27 12:38:20 +0530

    finalize the implementation

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=
 var server = http.createServer(function(request,response){
     var request = require('request');
     request(url,function(err,res,body){
-        console.log(body);
-        response.write('hi');
+        var data = JSON.parse(body);
+        response.write("<html><body><div id='container'>");
+        response.write("<h1>" + 'City Name : ' + data['name'] + '</br>' + "</h1>");
+        response.write("<h2>" + 'Temperature : ' + data.main['temp'] + '</br>' + "</h2>");
+        response.write("<h2>" + 'SunSet Time : ' + new Date(data.sys['sunset']*1000) + '</br>' + "</h2>");
+        response.write("</div></body></html>");
         response.end();
     });
 
